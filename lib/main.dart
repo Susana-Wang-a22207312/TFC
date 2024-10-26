4a1435fec467bca123332890e5978a7c6915b260
Susana <susana2004wang@outlook.pt>
2024-10-26 21:37:32 +0100

    added fav interaction and page

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,6 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-// ...
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -65,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -108,11 +107,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 }
-
-
-// ...
-
-
 
 class GeneratorPage extends StatelessWidget {
   @override
@@ -158,7 +152,6 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
-// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
@@ -186,6 +179,34 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
