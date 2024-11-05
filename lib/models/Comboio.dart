6f3ad7e5fb78d80ddc982b8703a4f5947dcd1432
Susana <susana2004wang@outlook.pt>
2024-11-05 23:01:14 +0000

    added the screens

--- lib/models/Comboio.dart
+++ lib/models/Comboio.dart
@@ -10,7 +10,7 @@ class Comboio {
 
   List<String> obterEstacoesLinhaSintra() {
     return [
-      'Lisboa - Rossio',
+      'Lisboa -Rossio',
       'Lisboa - Entrecampos',
       'Lisboa - Sete Rios',
       'Praça de Espanha',
